1c2b7ace7840b153bae2dab648d11e29efa9db34
ingmarolsson <olsson_ingmar@yahoo.se>
2015-08-17 21:50:52 +0200

    updated checkout instructions and removed bootstrap script

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# in case qmake && make was run before the boostrap script (with errors), delete the folder so we can check out the submodule
-rm -rf external/commonjs/plugin/
-
-git submodule update --init
